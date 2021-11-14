5afa045a0b3d6ce898989274cd818d368de8b467
ivedi <halil@veriyaz.com.tr>
2021-11-14 17:27:14 +0300

    added initialize func

--- pkg/main/controllers/simpleRedisController.go
+++ pkg/main/controllers/simpleRedisController.go
@@ -3,14 +3,17 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	// "github.com/ivedi/simple-go-redis/pkg/main/repositories"
 )
 
 type SimpleRedisController struct{}
 
 func (S SimpleRedisController) Get(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("hello darling")
+	// repositories.SimpleRedisRepository.Get("test")
 }
 
 func (S SimpleRedisController) Set(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("hello papa")
+	// repositories.SimpleRedisRepository.Set("test", "asdlfkj")
 }
