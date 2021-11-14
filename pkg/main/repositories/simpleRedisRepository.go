a9d4edd6d4c020bbf7e8e853051a4e41c271e81c
ivedi <halil@veriyaz.com.tr>
2021-11-14 18:01:21 +0300

    test changes

--- pkg/main/repositories/simpleRedisRepository.go
+++ pkg/main/repositories/simpleRedisRepository.go
@@ -5,14 +5,15 @@ type SimpleRedisRepository struct{}
 var inMemoryData map[string]string
 
 func (S SimpleRedisRepository) Initialize() {
-	inMemoryData = make(map[string]string)
+	// inMemoryData = make(map[string]string)
 }
 
-/*
 func (S SimpleRedisRepository) Get(key string) string {
-	return "test" //string(inMemoryData[key])
-}*/
+	inMemoryData = make(map[string]string)
+	inMemoryData["test"] = "helloppa4"
+	return inMemoryData["test"] //string(inMemoryData[key])
+}
 
 func (S SimpleRedisRepository) Set(key string, value string) {
-	inMemoryData[key] = value
+	// inMemoryData[key] = value
 }
